c17a8484d13471dfa4d23225f91964cbf5c97274
Hadrien Jouet <hadrien.jouet@grownseed.net>
2013-02-28 19:17:12 -0800

    updated sample config file with twitter details

--- config.sample.js
+++ config.sample.js
@@ -6,3 +6,12 @@ exports.port = 8080;
 
 //Session secret
 exports.secret = 'some secret phrase';
+
+//Twitter API
+exports.twitterAPI =
+{
+	consumer_key: 'xxxxxxxx',
+	consumer_secret: 'xxxxxxxx',
+	access_token_key: 'xxxxxxxx',
+	access_token_secret: 'xxxxxxxx'
+};
